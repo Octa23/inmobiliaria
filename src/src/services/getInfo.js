f462c729aae70a7c560fd0df4d16b2c89f0e40cb
Octa23 <octa233@gmail.com>
2021-10-11 12:43:48 -0300

    commit main

--- src/src/services/getInfo.js
+++ src/src/services/getInfo.js
@@ -1,24 +1,24 @@
-import axios from 'axios'
-import Papa from 'papaparse'
+import axios from "axios";
+import Papa from "papaparse";
 
 const getInfo = async () => {
   return axios
     .get(
-      'https://docs.google.com/spreadsheets/d/e/2PACX-1vQkMK9JhKKEkVSifYq-4Nwr4Xz1qrtb-6lSRESU2F0RVUYRfuy7_JmD4CzVCi_dVMHaY9l4zp9ZY59m/pub?gid=0&single=true&output=csv',
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vQkMK9JhKKEkVSifYq-4Nwr4Xz1qrtb-6lSRESU2F0RVUYRfuy7_JmD4CzVCi_dVMHaY9l4zp9ZY59m/pub?gid=0&single=true&output=csv",
       {
-        responseType: 'blob'
-      }
+        responseType: "blob",
+      },
     )
-    .then(response => {
+    .then((response) => {
       return new Promise((resolve, reject) => {
         Papa.parse(response.data, {
           header: true,
           complete: (results) => {
-            return resolve(results.data)
-          }
-        })
-      })
-    })
-}
+            return resolve(results.data);
+          },
+        });
+      });
+    });
+};
 
-export default getInfo
+export default getInfo;
